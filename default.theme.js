76a7e7de73c9984a0c42dcb51d1dd5b043048806
Alex Luong <alex@alexluong.com>
2022-07-12 13:33:38 +0700

    feat: add more color tokens

--- default.theme.js
+++ default.theme.js
@@ -2,14 +2,20 @@ const colors = require("tailwindcss/colors");
 
 module.exports = {
   colors: {
-    text: "#9396b2",
+    text: "#abadc6",
     "text-muted": "#abadc6",
     "text-important": "#dcdef3",
+    body: "#abadc6",
+    heading: "#fff",
     "input-placeholder": "rgba(220, 222, 243, 0.5)", // #dcdef3 50%
     "background-0": "#151825",
     "background-1": "#1a1d2d",
     "background-2": "#1e2235",
     "background-3": "#252a41",
+    "layer-0": "#151825",
+    "layer-1": "#1a1d2d",
+    "layer-2": "#1e2235",
+    "layer-3": "#252a41",
     primary: colors.blue["600"],
     "primary-accent": colors.blue["800"],
     critical: colors.red["600"],
